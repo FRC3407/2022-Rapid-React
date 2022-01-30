c7ab767ae8d6dc730c3d4215866504e689230fec
Sam Richter <hoodie888@outlook.com>
2022-01-30 17:12:21 -0600

    test?

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,18 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class Robot {
+	
+	public static class Intake extends SubsystemBase {
+		
+	}
+	public static class Transfer extends SubsystemBase {
+
+	}
+	public static class Shooter extends SubsystemBase {
+		
+	}
+
+}
