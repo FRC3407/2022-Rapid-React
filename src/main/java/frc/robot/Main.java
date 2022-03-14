9c1db2f03bda0d56eb33a3844cf3ae8995e2b069
Sam Richter <hoodie888@outlook.com>
2022-03-13 22:56:50 -0500

    clean

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -2,9 +2,11 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
-public final class Main {
-	public static void main(String... args) 
-		{ RobotBase.startRobot(Runtime::new); }
-		//	^^^ 'Robot::new' changed to 'Runtime::new', as this is the class that extends TimedRobot
+
+public class Main {
+
+	public static void main(String... args) {
+		RobotBase.startRobot(Robot::new);
+	}
 
 }
---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,30 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+
+public class Robot extends TimedRobot {
+
+	public Robot() {}
+
+	@Override public void robotInit() {}
+	@Override public void robotPeriodic() {}
+
+	@Override public void disabledInit() {}
+	@Override public void disabledPeriodic() {}
+	@Override public void disabledExit() {}
+
+	@Override public void teleopInit() {}
+	@Override public void teleopPeriodic() {}
+	@Override public void teleopExit() {}
+
+	@Override public void autonomousInit() {}
+	@Override public void autonomousPeriodic() {}
+	@Override public void autonomousExit() {}
+
+	@Override public void testInit() {}
+	@Override public void testPeriodic() {}
+	@Override public void testExit() {}
+
+
+}
